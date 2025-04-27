7ff5db41f62821552a15103d59e6924ccb9f3d4e
SimonKimon <niklaes2002@gmail.com>
2025-04-27 14:16:44 +0200

    Blatt 4 Mergesort A1 fertig

--- labor_loesung/de/hska/iwi/ads/solution/sorting/Mergesort.java
+++ labor_loesung/de/hska/iwi/ads/solution/sorting/Mergesort.java
@@ -6,7 +6,29 @@ public class Mergesort<E extends Comparable<E>> extends AbstractMergesort<E> {
 
     @Override
     protected void mergesort(E[] a, int left, int right) {
-
+        if (left < right) {
+            int m = (left + right) / 2;
+            mergesort(a, left, m);
+            mergesort(a, m + 1, right);
+            merge(a, super.b, left, m, right);
+        }
     }
 
+    private void merge(E[] a, E[] b, int left, int m, int right) {
+        int l = left;
+        int r = m + 1;
+        for (int i = left; i <= right; i++) {
+            if (r > right || ((l <= m) && (a[l].compareTo(a[r]) <= 0))) {
+                b[i] = a[l];
+                l++;
+            }
+            else {
+                b[i] = a[r];
+                r++;
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            a[i] = b[i];
+        }
+    }
 }
--- labor_loesung/de/hska/iwi/ads/solution/sorting/SortTest.java
+++ labor_loesung/de/hska/iwi/ads/solution/sorting/SortTest.java
@@ -1,4 +1,17 @@
 package de.hska.iwi.ads.solution.sorting;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import de.hska.iwi.ads.sorting.AbstractMergesort;
+import org.junit.jupiter.api.Test;
+
 public class SortTest {
+
+    @Test
+    void Test1() {
+        AbstractMergesort<Integer> mergesort = new Mergesort<>();
+        Integer[] a = {12, 47, 3, 29, 8, 34, 18, 1, 45, 22, 5, 31, 14, 48, 6, 24, 19, 0, 41, 36, 10, 17, 39, 7, 30, 49, 27, 2, 15, 44, 9, 20, 33, 43, 28, 13, 25, 46, 11, 37, 4, 26, 42, 21, 16, 38, 32, 23, 35, 50, 40};
+
+        mergesort.sort(a);
+    }
 }
